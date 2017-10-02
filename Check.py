595b126588742b20070c6ccfbffd52b5ddc3b8ae
Vijay <vijay.william@outlook.com>
2017-10-02 00:30:49 -0400

    Create Check.py

---
+++ Check.py
@@ -0,0 +1,12 @@
+def func1():
+ for i in range (1,100):
+  if i % 15 == 0:
+    print("VijayWilliam")
+  elif i % 5 ==0:
+    print("William")
+  elif i % 3 == 0:
+    print("Vijay")
+  else:
+    print i
+
+func1()
